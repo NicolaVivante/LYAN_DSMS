606a36928010b37a96e4833f12bc0a52657c5925
Luca <lucavaccari03@gmail.com>
2022-03-01 15:04:20 +0100

    Implemented JSON getUsers and getUserByName

--- DataBaseServer/js_server/index.js
+++ DataBaseServer/js_server/index.js
@@ -1,22 +1,50 @@
+const fs = require("fs").promises;
 const express = require("express");
 const app = express();
 app.use(express.json());
 const PORT = 5555;
 
+async function readfile(path) {
+  try {
+    return await fs.readFile(path);
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+}
+
+async function writeFile(path, content) {
+  try {
+    await fs.writeFile(path, content);
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+}
+
+const filePath = "./users.json";
 const JSONDataManager = {
-  getUsers: () => {
+  getUsers: async function () {
+    const data = await readfile(filePath);
+    return JSON.parse(data);
+  },
+  getUserByName: async (userName) => {
+    return await JSON.parse(await readfile(filePath)).filter(
+      (u) => u.userName == userName
+    )[0];
+  },
+  addUser: async (user) => {
     // TODO
   },
-  getUserByName: (userName) => {
-    // TODO
-  },
-  addUser: (user) => {
-    // TODO
-  },
-  setKey: (key, userName) => {
+  setKey: async (key, userName) => {
     // TODO
   },
 };
+
+async function sus() {
+  console.log(await JSONDataManager.getUserByName("luca"));
+}
+sus();
 
 const DBDataManager = {
   getUsers: () => {
@@ -35,30 +63,30 @@ const DBDataManager = {
 
 const DataManager = JSONDataManager;
 
-app.get("/users", (req, res) => {
+app.get("/users", async (req, res) => {
   // TODO: filters
-  res.json(DataManager.getUsers());
+  res.json(await DataManager.getUsers());
 });
 
-app.get("/verify", (req, res) => {
+app.get("/verify", async (req, res) => {
   res.json({
     verified:
-      DataManager.getUserByName(req.body.username).passwordDigest ==
+      (await DataManager.getUserByName(req.body.username).passwordDigest) ==
       req.body.passwordDigest,
   });
 });
 
-app.post("/users", (req, res) => {
-  DataManager.addUser(req.body);
+app.post("/users", async (req, res) => {
+  await DataManager.addUser(req.body);
   // TODO: send outcome
 });
 
-app.post("/key", (req, res) => {
+app.post("/key", async (req, res) => {
   // TODO
-  DataManager.setKey(req.body.key, req.body.userName);
+  await DataManager.setKey(req.body.key, req.body.userName);
   // TODO: send outcome
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log("LYAN server listening on port " + PORT);
 });
