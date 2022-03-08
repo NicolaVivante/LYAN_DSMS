fca12df1094ebe86f204d215f29e5eebe83ce829
NicolaVivante <57539526+NicolaVivante@users.noreply.github.com>
2022-03-08 21:54:22 +0100

    removed rsa.js

--- DataBaseServer/js_server/index.js
+++ DataBaseServer/js_server/index.js
@@ -5,9 +5,9 @@ app.use(express.json());
 const PORT = process.env.PORT;
 
 // Change the value of this variable to JSONDataManager or DBDataManager to swich between the two systems
-const DataManager = require("./js/JSONDataManager");
+// const DataManager = require("./js/JSONDataManager");
 
-console.log(require("./js/rsa").encrypt());
+// console.log(require("./js/rsa").encrypt());
 
 // API URLs
 app.get("/users", async (req, res) => {
